b7c73009fdfa4f9cef1269aa39163f2744a46333
BK <boro.kovac@gmail.com>
2016-11-28 19:56:01 +0100

    added delete functionality to /projects/delete/:id upon POST

--- controllers/projects/delete.js
+++ controllers/projects/delete.js
@@ -1,8 +1,38 @@
 'use strict'
 
+var db = require('../../database.js');
+
 module.exports = function(app) {
 
-	app.get('/projects/delete/:id', function(req, res) {
-		res.send('Not done yet.');
+	app.post('/projects/delete/:id', function(req, res) {
+		var id = req.params.id;
+		db.transaction(function(trx) {
+			// Note: make sure an address can be assigned to 1 project only or that no other project uses it
+			db('project_addresses')
+				.transacting(trx)
+				.where('project_id', id)
+				.del()
+				.catch(function(error) {
+					console.log(error);
+				})
+				.then(
+					db('projects')
+					.where('id', id)
+					.del()
+					.catch(function(error) {
+						console.log(error);
+					})
+				)
+				.then(trx.commit)
+				.catch(trx.rollback)
+				.catch(function(error) {
+					console.log(error);
+				})
+				.finally(function() {
+					res.redirect('../../')
+
+				})
+		})
+
 	});
 };
